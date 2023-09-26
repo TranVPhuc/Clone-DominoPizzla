b3622eae0b2531b003d54a7d612acf56fc743867
TranVPhuc <tranvanphuc.2712@gmail.com>
2023-09-26 16:01:48 +0700

    Add icon

--- src/Components/AdminPage/Admin.js
+++ src/Components/AdminPage/Admin.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import { LaptopOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import './fontawesome-free-6.4.0-web/css/all.min.css';
 import { Layout, Menu, Button, theme, Avatar } from 'antd';
 import './Admin.css';
 import Homepage1 from '../pages/Homepage/Homepage1';
@@ -19,6 +20,7 @@ const Admin = (props) => {
   const listMenu = [
     {
       key: 'home',
+      icon: <i class="fa-solid fa-house"></i>,
       label: 'Home',
     },
     {
@@ -26,6 +28,7 @@ const Admin = (props) => {
     },
     {
       key: 'allproduct',
+      icon: <i class="fa-solid fa-cart-shopping"></i>,
       label: 'All Products',
     },
     {
@@ -35,34 +38,42 @@ const Admin = (props) => {
     },
     {
       key: 'mice',
+      icon: <i class="fa-sharp fa-solid fa-computer-mouse"></i>,
       label: 'Mice',
     },
     {
       key: 'chair',
+      icon: <i class="fa-sharp fa-solid fa-chair"></i>,
       label: 'Chair',
     },
     {
       key: 'keyboard',
+      icon: <i class="fa-solid fa-keyboard"></i>,
       label: 'Keyboard',
     },
     {
       key: 'audio',
+      icon: <i class="fa-solid fa-headphones"></i>,
       label: 'Audio',
     },
     {
       key: 'console',
+      icon: <i class="fa-solid fa-game-console-handheld"></i>,
       label: 'Console',
     },
     {
       key: 'mobile',
+      icon: <i class="fa-solid fa-mobile"></i>,
       label: 'Mobile',
     },
     {
       key: 'apparel',
+      icon: <i class="fa-solid fa-shirt"></i>,
       label: 'Apparel',
     },
     {
       key: 'content',
+      icon: <i class="fa-solid fa-paintbrush-pencil"></i>,
       label: 'Content Creation',
     },
   ];
